5901eea3a60a818b0066b8cc4c9d9379516c1f31
Xiaohan He <2378444h@student.gla.ac.uk>
2019-02-04 05:46:38 +0800

    update chapter8

--- rango/forms.py
+++ rango/forms.py
@@ -32,16 +32,15 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
             return cleaned_data
-
     class Meta:
-        # Provide an association between the ModelForm and a model
+    # Provide an association between the ModelForm and a model
         model = Page
 
-        # What fields do we want to include in our form?
-        # This way we don't need every field in the model present.
-        # Some fields may allow NULL values, so we may not want to include them.
-        # Here, we are hiding the foreign key.
-        # we can either exclude the category field from the form,
+    # What fields do we want to include in our form?
+    # This way we don't need every field in the model present.
+    # Some fields may allow NULL values, so we may not want to include them.
+    # Here, we are hiding the foreign key.
+    # we can either exclude the category field from the form,
         exclude = ('category',)
-        # or specify the fields to include (i.e. not include the category field)
-        #fields = ('title', 'url', 'views')
+    # or specify the fields to include (i.e. not include the category field)
+    #fields = ('title', 'url', 'views')
